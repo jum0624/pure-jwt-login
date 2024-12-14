6c2b7ad6ea14c9b4c029d3ecc104a1a9556dd35f
Eunmi <jum0624@koriemsoft.com>
2024-12-14 17:14:34 +0900

    init project

---
+++ src/test/java/com/example/purejwtlogin/PureJwtLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.purejwtlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PureJwtLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
